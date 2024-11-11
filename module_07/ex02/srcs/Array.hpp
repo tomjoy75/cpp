206a8f1f6988ebd7452051f88610cfa61a5c6882
tomjoy75 <tomjoy75@gmail.com>
2024-11-11 18:55:11 +0100

    ex02 ..work in progress

--- module_07/ex02/srcs/Array.hpp
+++ module_07/ex02/srcs/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: tjoyeux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 17:13:22 by tjoyeux           #+#    #+#             */
-/*   Updated: 2024/11/10 17:54:22 by tjoyeux          ###   ########.fr       */
+/*   Updated: 2024/11/11 18:52:02 by tjoyeux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <iostream>
 # include <string>
+# include <stdexcept>
 
 # define RED "\033[0;31m"
 # define GREEN "\033[0;32m"
@@ -27,16 +28,56 @@
 
 template <typename T>
 class	Array{
+
 public:
 	Array( void ) : _tab(NULL), _n(0){};
-	Array( unsigned int n ) : _n(n){};
-	~Array( void ){};
+	Array( unsigned int n ) : _n(n){
+		_tab = new T[n]();	
+	};
+	Array(Array const &src): _n(src._n){
+		this->_tab = new T[_n]();
+		for (int i = 0; i < _n; i++)
+//			this->_tab[i] = src.getElem(i);	
+			this->_tab[i] = src._tab[i];	
+	};
+	~Array( void ){
+		delete [](_tab);	
+	};
 
-	int	size( void ){return (_n);};
+	Array &operator=(Array const &rhs){
+		if ( this != &rhs ){
+			delete [](this->_tab);
+			this->_n = rhs.size();
+			this->_tab = new T[this->_n];
+			for (int i = 0; i < this->_n; i++)
+	//			this->_tab[i] = rhs.getElem(i);	
+				this->_tab[i] = rhs._tab[i];
+		}
+		return (*this);
+	}
+
+	T const &operator[]( int idx) const{
+		if (idx >= static_cast<int>(_n) || idx < 0)
+			throw std::out_of_range("Out of range value");
+		return (_tab[idx]);
+	};
+
+	int	size( void ) const {return (_n);};
+
+//	T	&getElem( unsigned int i ) const {
+//		if (i >= this->_n )
+//			throw std::out_of_range("Out of range value");		
+//		return _tab[i];
+//	};
+	void	display(){
+		for (int i = 0; i < _n; i++){
+			std::cout << "element " << i << " : " << _tab[i] << std::endl;
+		}
+	}
+
 private:
-	T	*_tab;
-	int	_n;
-
+	T				*_tab;
+	unsigned int	_n;
 };
 
 #endif
--- module_07/ex02/srcs/main.cpp
+++ module_07/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tjoyeux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 17:18:05 by tjoyeux           #+#    #+#             */
-/*   Updated: 2024/11/10 17:52:21 by tjoyeux          ###   ########.fr       */
+/*   Updated: 2024/11/11 18:53:47 by tjoyeux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,26 @@
 int	main( void ){
 
 	Array<int>	*empty = new Array<int>();
+	Array<int>	*param = new Array<int>(5);
+	int	i = 0;
+//	std::string * a = new std::string();
+//	std::cout << *a << std::endl;
+//	std::string * b = new std::string[2]();
+//	std::cout << b[1] << std::endl;
 	std::cout << "size : " << empty->size() << std::endl;
+	empty->display();
+	std::cout << "size : " << param->size() << std::endl;
+	param->display();
+	for (; i < 5 ; i++)
+		std::cout << "elem " << i + 1 << " : " << param->_tab[i] << std::endl;	
+	i++;
+	try{
+		std::cout << "elem " << i + 1 << " : " << param->_tab[i] << std::endl;	
+	}
+	catch (std::out_of_range e){
+		std::cout << e.what() << std::endl;
+	}
 	delete (empty);
+	delete (param);
 	return ( 0 );
 }
