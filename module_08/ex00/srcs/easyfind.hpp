da89164f4d98275d9d25ecc870df4ffcaf84a6b9
tomjoy75 <tomjoy75@gmail.com>
2024-11-26 13:58:03 +0100

    mod 8 ex 00 completed

--- module_08/ex00/srcs/easyfind.hpp
+++ module_08/ex00/srcs/easyfind.hpp
@@ -25,15 +25,10 @@
 # define ITALIC "\033[3m"
 # define BLINK "\e[5m"
 
-//int	easyfind(T const &numbers, int num){
 template < typename T > // T should be an int container
 typename T::const_iterator	easyfind(T const &numbers, int num){
 	typename T::const_iterator	it;	
 	it = std::find(numbers.begin(), numbers.end(), num);
-	/*if (it != numbers.end())*/
-	/*	std::cout << "Founded : " << *it << std::endl;*/
-	/*else*/
-	/*	std::cout << "Not Found " << std::endl;*/
 	return (it);
 }
 
--- module_08/ex00/srcs/main.cpp
+++ module_08/ex00/srcs/main.cpp
@@ -17,28 +17,80 @@
 
 int main(void){
 	int	tab[] = {42, 53, 0, 21};	
+
+// Vector
 	std::vector<int>	vec(tab, tab + sizeof(tab) / sizeof(int));  
-	std::vector<int>::const_iterator	it;
+	std::vector<int>::const_iterator	itV;
 
 	std::cout << BLUE << "With a vector" << RESET << std::endl;
-	it = easyfind(vec, 42);
-	if (it != vec.end())
-		std::cout << "Founded : " << *it << std::endl;
+	itV = easyfind(vec, 42);
+	if (itV != vec.end())
+		std::cout << "Founded : " << *itV << std::endl;
 	else
 		std::cout << "Not found !" << std::endl;
-	it = easyfind(vec, 0);
-	if (it != vec.end())
-		std::cout << "Founded : " << *it << std::endl;
+	itV = easyfind(vec, 0);
+	if (itV != vec.end())
+		std::cout << "Founded : " << *itV << std::endl;
 	else
 		std::cout << "Not found !" << std::endl;
-	it = easyfind(vec, 21);
-	if (it != vec.end())
-		std::cout << "Founded : " << *it << std::endl;
+	itV = easyfind(vec, 21);
+	if (itV != vec.end())
+		std::cout << "Founded : " << *itV << std::endl;
 	else
 		std::cout << "Not found !" << std::endl;
-	it = easyfind(vec, 84);
-	if (it != vec.end())
-		std::cout << "Founded : " << *it << std::endl;
+	itV = easyfind(vec, 84);
+	if (itV != vec.end())
+		std::cout << "Founded : " << *itV << std::endl;
+	else
+		std::cout << "Not found !" << std::endl;
+//Deque
+	std::cout << BLUE << "With a deque" << RESET << std::endl;
+	std::deque<int>		dec(tab, tab + sizeof(tab) / sizeof(int));
+	std::deque<int>::const_iterator		itD;
+
+	itD = easyfind(dec, 42);
+	if (itD != dec.end())
+		std::cout << "Founded : " << *itD << std::endl;
+	else
+		std::cout << "Not found !" << std::endl;
+	itD = easyfind(dec, 0);
+	if (itD != dec.end())
+		std::cout << "Founded : " << *itD << std::endl;
+	else
+		std::cout << "Not found !" << std::endl;
+	itD = easyfind(dec, 21);
+	if (itD != dec.end())
+		std::cout << "Founded : " << *itD << std::endl;
+	else
+		std::cout << "Not found !" << std::endl;
+	itD = easyfind(dec, 84);
+	if (itD != dec.end())
+		std::cout << "Founded : " << *itD << std::endl;
+	else
+		std::cout << "Not found !" << std::endl;
+// List
+	std::cout << BLUE << "With a list" << RESET << std::endl;
+	std::list<int>		list(tab, tab + sizeof(tab) / sizeof(int));
+	std::list<int>::const_iterator		itL;
+
+	itL = easyfind(list, 42);
+	if (itL != list.end())
+		std::cout << "Founded : " << *itL << std::endl;
+	else
+		std::cout << "Not found !" << std::endl;
+	itL = easyfind(list, 0);
+	if (itL != list.end())
+		std::cout << "Founded : " << *itL << std::endl;
+	else
+		std::cout << "Not found !" << std::endl;
+	itL = easyfind(list, 21);
+	if (itL != list.end())
+		std::cout << "Founded : " << *itL << std::endl;
+	else
+		std::cout << "Not found !" << std::endl;
+	itL = easyfind(list, 84);
+	if (itL != list.end())
+		std::cout << "Founded : " << *itL << std::endl;
 	else
 		std::cout << "Not found !" << std::endl;
 
