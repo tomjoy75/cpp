7dc76f88921fcd494a5d69fc892db4bb904756c6
tomjoy75 <tomjoy75@gmail.com>
2024-11-27 16:19:26 +0100

    Exo 2 __ Functions implemented __
    Work on exceptions

--- module_08/ex01/srcs/Span.cpp
+++ module_08/ex01/srcs/Span.cpp
@@ -16,15 +16,42 @@ Span::Span( void ):_n(0){};
 
 Span::Span( unsigned int N ):_n(N){};
 
+void		showContent(std::multiset<int> set){
+	std::multiset<int>::const_iterator	it = set.begin();
+	for (; it != set.end(); it++)
+		std::cout << "item : " << *it << std::endl;
+
+}
+
 //TODO: Creer exception si N depasse
 void 		Span::addNumber( int N ){
-	this->_data.insert(N);
+	if (this->_data.size() < this->_n)
+		this->_data.insert(N);
 };
 
 unsigned int	Span::shortestSpan( void ){
-	return( 42 );
+	showContent(this->_data);
+	std::multiset<int>::const_iterator	it = this->_data.begin();
+	std::multiset<int>::const_iterator	next_it = it;
+	++next_it;
+	int				lower = 0;
+
+	if (next_it != this->_data.end())
+		lower = *next_it - *it; 
+	std::cout << "TEST lower : " << lower << std::endl;
+	//else throw an exception
+	while (next_it != this->_data.end()){
+		if (*next_it - *it < lower)
+			lower = *next_it - *it;
+		it++;
+		next_it++;
+	}
+	return( lower );
 };
 
+//TO DO creer exception
 unsigned int	Span::longestSpan( void ){
-	return( 42 );
+	std::cout << "Begin : " << *(this->_data.begin()) << std::endl;
+	std::cout << "End : " << *(--this->_data.end()) << std::endl;
+	return( *(--this->_data.end()) - *(this->_data.begin()) );
 };
